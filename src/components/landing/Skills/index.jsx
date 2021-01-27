566a05dcdacf2763c28236070ab6468a0cc83f76
marina <nicolaimarina@gmail.com>
2021-01-27 13:09:38 -0500

    update of education

--- src/components/landing/Skills/index.jsx
+++ src/components/landing/Skills/index.jsx
@@ -11,11 +11,15 @@ export const Skills = () => {
     <Wrapper id="about">
       <SkillsWrapper as={Container}>
         <Details theme={theme}>
-          <h1>More about me</h1>
-          <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-            industry’s standard dummy.
-          </p>
+          <h1>My path:</h1>
+          <ul>
+            <li><span>2016</span> started teaching myself to code thru the different platforms available</li>
+            <li><span>2017</span> started teaching myself the cybersecurity</li>
+            <li><span>2018</span> passed the Security+ by CompTIA certification</li>
+            <li><span>August 2019 - July 2020</span> worked as a junior web developer for 11 months</li>
+            <li><span> July 2020</span> got laid off due to CORVID cuts</li>
+            <li><span>Fall 2020</span> back to self teaching my self and also decided that this is what I want to do till the rest of my life, so I am earing online the B.S. in Computer Science</li>
+          </ul>
           <Button as={AnchorLink} href="#contact">
             Hire me
           </Button>
