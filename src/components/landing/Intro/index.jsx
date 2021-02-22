499c8f9203070992af666439e1c40e9fd5342970
marina <nicolaimarina@gmail.com>
2021-02-22 15:07:51 -0500

    ading the wave-emoji

--- src/components/landing/Intro/index.jsx
+++ src/components/landing/Intro/index.jsx
@@ -5,6 +5,8 @@ import { Header } from '../../../components/theme';
 import { Button } from '../../common/Button';
 import { Container } from '../../common/Container';
 import { Wrapper, IntroWrapper, Details } from './styles';
+import emoji from "react-easy-emoji";
+import "./styles.css";
 
 export const Intro = () => {
   const { theme } = useContext(ThemeContext);
@@ -14,8 +16,9 @@ export const Intro = () => {
       <Header />
       <IntroWrapper as={Container}>
         <Details theme={theme}>
-          <h1>Hello!</h1>
-          <h4>I’m Marina Nicolai and I'm a JS developer!</h4>
+          <h1>Hi all, I'm Marina <span className="wave-emoji">{emoji("👋")}</span> </h1>
+          <p>A passionate Front-End Developer having an experience of building Web
+            and Mobile applications with JavaScript / Reactjs and some other libraries and frameworks.</p>
           <Button as={AnchorLink} href="#contact">
             Hire me
           </Button>
